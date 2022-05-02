f0bcc7a02a6790e562f1e09a46dcddfbca0b14e7
Jill Cunningham <jleighcunningham@gmail.com>
2022-05-01 22:03:56 -0400

    created js to generate team profiles

---
+++ dist/profileGenerator.js
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profile Generator</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://kit.fontawesome.com/f1439bb7b6.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 jumbotron mb-3 team-heading">
+          <h1 class="text-center">Team Profile Generator</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="team-area col-12 d-flex justify-content-center">
+          <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">Jill Cunningham</h2>
+        <h3 class="card-title"><i class="fas fa-user-astronaut"></i>Manager</h3>
+    </div>
+    <div class="card-body">
+        <ul class="collection">
+            <li class="collection-item">ID: 001</li>
+            <li class="collection-item">Email: <a href="mailto:jcunningham@gmail.com">jcunningham@gmail.com</a> </li>
+            <li class="collection-item">Office Number: 860-000-0000</li>
+        </ul>
+    </div>
+</div><div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">Milagros Johnson</h2>
+        <h3 class="card-title"><i class="fas fa-wrench"></i>Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="collection">
+            <li class="collection-item">ID: 002</li>
+            <li class="collection-item">Email: <a href="mailto:mjohnson@gmail.com">mjohnson@gmail.com</a> </li>
+            <li class="collection-item">Github: <a href="https://github.com/mjohnson" target="_blank" rel="noopener noreferrer">mjohnson</a></li>
+        </ul>
+    </div>
+</div><div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">Donavan Capers</h2>
+        <h3 class="card-title"><i class="fas fa-pen"></i>Intern</h3>
+    </div>
+    <div class="card-body">
+        <ul class="collection">
+            <li class="collection-item">ID: 003</li>
+            <li class="collection-item">Email: <a href="mailto:dcapers@gmail.com">dcapers@gmail.com</a> </li>
+            <li class="collection-item">School: UCONN</li>
+        </ul>
+    </div>
+</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
